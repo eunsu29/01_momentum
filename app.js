353abb40d71c8a678a97c55355a57f23fb06d140
Eunsu <edit1643@gmail.com>
2024-10-12 15:40:36 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -3,14 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 새로고침 방지
     const username = loginInput.value;
-    localStorage.setItem("username", username); // localStorage에 저장, getItem으로 불러오기 가능
+    localStorage.setItem(USERNAME_KEY, username); // localStorage에 저장, getItem으로 불러오기 가능
     loginForm.classList.add(HIDDEN_CLASSNAME);
-    greeting.innerText = `Hello ${username}`; // `` 사용, String + 변수
-    greeting.classList.remove(HIDDEN_CLASSNAME);
+    paintGreeting(username);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit); // 정보를 담은채로 function 호출
+function paintGreeting(username) {
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit); // 정보를 담은채로 function 호출
+} else {
+    paintGreeting(savedUsername);
+}
