135a9ddd226dfad636425069c21a256ead2b865b
Eunsu <edit1643@gmail.com>
2024-10-12 14:51:15 +0900

    4.3 Events pt.2

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault(); // 새로고침 방지
     console.log(loginInput.value);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit); // 정보를 담은채로 function 호출
+link.addEventListener("click", handleLinkClick);
