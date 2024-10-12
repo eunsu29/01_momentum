7e00dc31e9af0226872f39417ccd31ac39eab8be
Eunsu <edit1643@gmail.com>
2024-10-12 14:44:20 +0900

    4.2 Events

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function onLoginBtnClick() {
-    const username = loginInput.value;
-    if (username === "") {
-        alert("Please write your name.");
-    } else if (username.length > 15) {
-        alert("Your name is too long.");
-    }
+function onLoginSubmit(event) {
+    event.preventDefault(); // 새로고침 방지
+    console.log(loginInput.value);
 }
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit); // 정보를 담은채로 function 호출
