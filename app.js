8373953e49d90efd664ea3046151bdaf9e955ebc
Eunsu <edit1643@gmail.com>
2024-10-12 14:33:45 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
-    const name = loginInput.value;
-    console.log(name);
+    const username = loginInput.value;
+    if (username === "") {
+        alert("Please write your name.");
+    } else if (username.length > 15) {
+        alert("Your name is too long.");
+    }
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
