a50c651b409ababa78905d920991bf01394121ba
Eunsu <edit1643@gmail.com>
2024-10-12 14:19:04 +0900

    Delete 2.15 condition.js

--- 2.15 condition.js
+++
@@ -1,9 +0,0 @@
-const age = parseInt(prompt("age?"));
-
-if(isNaN(age) || age < 0) {
-    console.log("write positive number")
-} else if(age >= 20) {
-    console.log("drink")
-} else {
-    console.log("don't drink")
-}
