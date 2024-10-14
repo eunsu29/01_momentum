66383f5b6ad452d13077ea15016ed56f1108200f
Eunsu <edit1643@gmail.com>
2024-10-14 23:57:09 +0900

    5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -1,9 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
+function getClock() {
+    const date = new Date(); // new Date() 날짜를 가져옴
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
 
-// setInterval(sayHello, 1000); // ms마다 실행
+getClock();
+setInterval(getClock, 1000); // ms마다 실행
 
-setTimeout(sayHello, 1000); // ms후에 1번 실행
+// setTimeout(getClock, 1000); // ms후에 1번 실행
