0dd3a239273dc1a00f61a351660b881e60a6376c
Eunsu <edit1643@gmail.com>
2024-10-15 01:37:00 +0900

    7.2 Deleting ToDos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,23 @@ const todoForm = document.querySelector("#todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector("#todo-list");
 
+function deleteTodo(event) {
+    const li = event.target.parentElement; // 클릭한 대상의 부모요소 확인가능
+    li.remove(); // 삭제
+}
+
 function paintTodo(newTodo) {
     const li = document.createElement("li");
+
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+
+    li.appendChild(span);
+    li.appendChild(button);
     todoList.appendChild(li);
 }
 
