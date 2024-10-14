f305b87dc1e116b12a71857b27e58c9e39f78914
Eunsu <edit1643@gmail.com>
2024-10-15 00:04:41 +0900

    5.2 PadStart

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,10 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date(); // new Date() 날짜를 가져옴
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0"); // string.padStart(원하는길이, 채울문자)
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 getClock();
